982c5e72cb9cc4086448e2270d5686a2930a5b9d
= <=dave.jodatek@gmail.com>
2023-06-17 11:13:30 +0100

    first commit

---
+++ src/main/java/com/gomycode/syrinx/SyrinxApplication.java
@@ -0,0 +1,13 @@
+package com.gomycode.syrinx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyrinxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyrinxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gomycode/syrinx/SyrinxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomycode.syrinx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyrinxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
